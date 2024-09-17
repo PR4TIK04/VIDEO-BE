ea297faaa48d80752dc43d7dc2e37592abf83917
Pratik Chauhan <chauhanpratik21444@student.sfit.ac.in>
2024-09-17 15:36:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+const cors = require('cors');
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, { cors: { origin: '*' } });
+
+app.use(cors());
 
 io.on('connection', (socket) => {
   console.log('New client connected');
 
   socket.on('viewer-request', (signal) => {
-    // Relay viewer request to the host
     io.emit('viewer-request', signal);
   });
 
   socket.on('host-response', (signal) => {
-    // Relay host response back to the viewer
     io.emit('host-response', signal);
   });
 
